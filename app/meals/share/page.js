7740d0c72a37a37fd48367ceb3ea66771ca030f8
Mani Sohal <manisohal837@gmail.com>
2023-04-22 19:37:35 +0530

    feat: add server action for Share page form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
 import ImagePicker from '@/components/Meals/ImagePicker';
 
+import { shareMeal } from '@/lib/actions';
+
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -12,7 +14,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -35,7 +37,7 @@ export default function ShareMealPage() {
             <label htmlFor="instructions">Instructions</label>
             <textarea id="instructions" name="instructions" rows="10" required></textarea>
           </p>
-          <ImagePicker  />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
