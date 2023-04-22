8015bc7c153628f3b256bac23c5930b15f537afc
Mani Sohal <manisohal837@gmail.com>
2023-04-22 15:48:51 +0530

    feat: fetch and display meals via sqlite db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 
+import { getMeals } from '@/lib/meals';
+
 import MealsGrid from '@/components/Meals/MealsGrid';
 
 import classes from './page.module.css';
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +21,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
